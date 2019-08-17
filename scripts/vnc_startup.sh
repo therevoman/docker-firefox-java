fb5e6f27d13860fa09b73acbb68e2226a4104e73
Nate Revo <nrevo@revoweb.com>
2019-08-17 01:39:05 -0600

    Fixes for it to work with java 6

--- scripts/vnc_startup.sh
+++ scripts/vnc_startup.sh
@@ -2,18 +2,23 @@
 
 #resolve_vnc_connection
 VNC_IP=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
-VNC_PORT="590"${DISPLAY:0}
-NO_VNC_PORT="690"${DISPLAY:0}
+#VNC_PORT="590"${DISPLAY:0}
+VNC_PORT="5900"
+#NO_VNC_PORT="690"${DISPLAY:0}
 
 ##change vnc password
 echo "change vnc password!"
-(echo $VNC_PW && echo $VNC_PW) | vncpasswd
+#(echo $VNC_PW && echo $VNC_PW) | x11vnc -storepasswd in /etc/x11vnc.pass
 
 ##start vncserver and noVNC webclient
-$NO_VNC_HOME/utils/launch.sh --vnc $VNC_IP:$VNC_PORT --listen $NO_VNC_PORT &
+#$NO_VNC_HOME/utils/launch.sh --vnc $VNC_IP:$VNC_PORT --listen $NO_VNC_PORT &
+$NO_VNC_HOME/utils/launch.sh --vnc $VNC_IP:$VNC_PORT  &
 #vncserver -kill :1 && rm -rfv /tmp/.X* ; echo "remove old vnc locks to be a reattachable container"
 #vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION
-sleep 1
+#x11vnc -create -forever -rfbauth ~/.vnc/passwd -geometry $VNC_RESOLUTION -o /root/.vnc/main_display.log &
+x11vnc -create -forever -nopw -geometry $VNC_RESOLUTION -o /root/.vnc/main_display.log &
+
+sleep 5
 ##log connect options
 echo -e "\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
@@ -25,7 +30,7 @@ case $i in
     # if option `-t` or `--tail-log` block the execution and tail the VNC log
     -t|--tail-log)
     echo -e "\n------------------ /root/.vnc/*$DISPLAY.log ------------------"
-    tail -f /root/.vnc/*$DISPLAY.log
+    tail -f /root/.vnc/*_display.log
     ;;
     *)
     # unknown option ==> call command
